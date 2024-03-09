f87bd305dcf9acc4b675e7831b8c80aa8d88b32a
shouya31 <shouya0331@ezweb.ne.jp>
2024-03-09 15:12:00 +0900

    Display 'Hello React!'

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,15 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (<h1>Hello React!</h1>);
+}
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root'),
+);
